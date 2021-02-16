e2ffc3bcc883fad61ccae7cf6aab52ff8ca573eb
fuchsvit <78554563+fuchsvit@users.noreply.github.com>
2021-02-16 19:10:27 +0300

    Update lamp_1.sh

--- lamp_1.sh
+++ lamp_1.sh
@@ -9,23 +9,23 @@
  sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert -y  
    
 ## Install PHP  
- apt-get install php libapache2-mod-php php-mysql -y  
+ sudo apt-get install php libapache2-mod-php php-mysql -y  
    
 # Install MySQL database server  
  export DEBIAN_FRONTEND="noninteractive"  
  debconf-set-selections <<< "mysql-server mysql-server/root_password password $db_root_password"  
  debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $db_root_password"  
- apt-get install mysql-server -y  
+ sudo apt-get install mysql-server -y  
    
 # Enabling Mod Rewrite  
  sudo a2enmod rewrite  
 
 # Install WordPress
 # Download archive
- wget -c http://wordpress.org/latest.tar.gz
+ sudo wget -c http://wordpress.org/latest.tar.gz
 
 # Unpack archive
- tar -xzvf latest.tar.gz
+ sudo tar -xzvf latest.tar.gz
 
 # Copy files to /var/www/html/:
  sudo rsync -av wordpress/* /var/www/html/
