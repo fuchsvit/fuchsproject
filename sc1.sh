752e9a80ce620e9cbee744fe60db4462d11d4a22
fuchsvit <78554563+fuchsvit@users.noreply.github.com>
2021-02-25 21:41:55 +0300

    Update sc1.sh

--- sc1.sh
+++ sc1.sh
@@ -2,29 +2,31 @@
 
 # This script should be executed on Linux Ubuntu Virtual Machine
 
-S=$(server)
+S=server
+C=client
 DATA_DIRECTORY=${2:-/data}
 
 echo "Updating packages"
-apt-get -y update
+sudo apt-get -y update
 
 if grep $S /etc/hosts 
 
 then echo "Installing NFS kernel server"
-apt-get -y install nfs-kernel-server
+sudo apt-get -y install nfs-kernel-server
 sudo systemctl enable nfs-server
 echo "Appending localhost to exports configuration file"
 echo "/data 192.168.100.0/24(rw,async,insecure,fsid=0,crossmnt,no_subtree_check)" >> /etc/exports
 
-else echo "Installing NFS kernel client"
-apt-get -y install nfs-common
+elif grep $C /etc/hosts
+then echo "Installing NFS client"
+sudo apt-get -y install nfs-common
 
 fi
 
 echo "Making data directory ${DATA_DIRECTORY}"
-mkdir -p ${DATA_DIRECTORY}
+sudo mkdir -p ${DATA_DIRECTORY}
 
-nohup service nfs-kernel-server restart
+sudo nohup service nfs-kernel-server restart
 
 
 
